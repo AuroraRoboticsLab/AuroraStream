709971bc31bb226516ade8d367deea957f83934b
Orion Lawlor <lawlor@alaska.edu>
2026-06-16 14:49:04 -0800

    Fix bug for v4l2 collection (add -d parameter to device name).

--- camera_profiles/collect_profile.sh
+++ camera_profiles/collect_profile.sh
@@ -12,7 +12,7 @@ mkdir "$name" || exit
 
 cd "$name"
 echo "Profile for $name as device $device" | tee NOTES.txt || exit 1
-log v4l2-ctl --list-formats "$device"  | tee -a NOTES.txt
+log v4l2-ctl --list-formats -d "$device"  | tee -a NOTES.txt
 log ffmpeg -f video4linux2 -list_formats all -i "$device" 2>&1 | grep "v4l2"  | tee -a NOTES.txt
-log v4l2-ctl --list-formats-ext "$device"  | tee -a NOTES.txt
+log v4l2-ctl --list-formats-ext -d "$device"  | tee -a NOTES.txt
  
