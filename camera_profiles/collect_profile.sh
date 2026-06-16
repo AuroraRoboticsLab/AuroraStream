3a217d62ba1f84366ff14c91f596b732d03c84d9
Orion Lawlor <lawlor@alaska.edu>
2026-06-16 14:21:28 -0800

    Add script to collect basic camera format and framerate data.

---
+++ camera_profiles/collect_profile.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Collect a camera profile
+name="$1"
+device="$2"
+
+log() {
+	echo "\n$@" || exit 1
+	"$@" || exit 1
+}
+
+mkdir "$name" || exit
+
+cd "$name"
+echo "Profile for $name as device $device" | tee NOTES.txt || exit 1
+log v4l2-ctl --list-formats "$device"  | tee -a NOTES.txt
+log ffmpeg -f video4linux2 -list_formats all -i "$device" 2>&1 | grep "v4l2"  | tee -a NOTES.txt
+log v4l2-ctl --list-formats-ext "$device"  | tee -a NOTES.txt
+ 
